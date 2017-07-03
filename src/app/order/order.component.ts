0b1177e4195d75e6b737de9f901cfe33899fcb3b
Rodrigo <rcbm539@gmail.com>
2017-07-03 11:08:28 -0300

    Criando validações personalizadas

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FormGroup, FormBuilder } from '@angular/forms'
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms'
 
 import { RadioOption } from '../shared/radio/radio-option.model'
 
@@ -19,6 +19,10 @@ import { Router } from '@angular/router'
 })
 export class OrderComponent implements OnInit {
 
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+  numberPattern = /^[0-9]*$/
+
   orderForm: FormGroup
 
   delivery: number = 8
@@ -36,15 +40,28 @@ export class OrderComponent implements OnInit {
 
   ngOnInit() {
     this.orderForm = this.formBuilder.group({
-      name: this.formBuilder.control(''),
-      email: this.formBuilder.control(''),
-      emailConfirmation: this.formBuilder.control(''),
-      address: this.formBuilder.control(''),
-      number: this.formBuilder.control(''),
+      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      email: this.formBuilder.control('',[Validators.required, Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.formBuilder.control('',[Validators.required, Validators.pattern(this.emailPattern)]),
+      address: this.formBuilder.control('',[Validators.required, Validators.minLength(5)]),
+      number: this.formBuilder.control('',[Validators.required, Validators.pattern(this.numberPattern)]),
       optionalAddress: this.formBuilder.control(''),
-      paymentOption: this.formBuilder.control('')
+      paymentOption: this.formBuilder.control('',[Validators.required])
       
-    })
+    },{validator: OrderComponent.equalsTo})
+  }
+
+  static equalsTo(group: AbstractControl): {[key: string]: boolean} {
+    const email = group.get('email')
+    const emailConfirmation = group.get('emailConfirmation')
+
+    if(!email || !emailConfirmation)
+      return undefined
+    
+    if(email.value !== emailConfirmation.value){
+      return { emailsNotMatch: true }
+    }
+    return undefined
   }
 
   itemsValue(): number {
